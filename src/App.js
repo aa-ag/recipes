f1b5324d933ffe9d7f4646caecf6ea6b312c9f1f
aa-ag <aaron.aguerrevere@gmail.com>
2020-08-25 17:26:43 -0500

    Ingredients

--- src/App.js
+++ src/App.js
@@ -7,26 +7,48 @@ const App = () => {
   const APP_KEY = '8ce7f8a6293a26260d9ee2db4ac7fa0d';
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("")
 
   useEffect( () =>{
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
-    console.log(data.hits)
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
   };
 
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text"/>
+      <small>Input an ingredient here.</small>
+      <form className="search-form" onSubmit={getSearch}>
+        <input 
+          className="search-bar" 
+          type="text" 
+          value={search} onChange={updateSearch}
+        />
         <button className="search-button" type="submit">Search</button>
       </form>
       {recipes.map(recipe => (
-        <Recipe/>
+        <Recipe
+        key={recipe.recipe.label}
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories} 
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
+        />
       ))};
     </div>
   );
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const Recipe = () => {
+const Recipe = ({title, calories, image, ingredients}) => {
     return (
         <div>
-            <h1>Title</h1>
-            <p>Calory count</p>
-            <img src="" alt="" />
+            <h1>{title}</h1>
+            <p>Calories: {calories}</p>
+            <img src={image} alt="" />
+            <ol>
+                {ingredients.map(ingridient => (
+                    <li>{ingridient.text}</li>
+                ))}
+            </ol>
         </div>
     );
 };
